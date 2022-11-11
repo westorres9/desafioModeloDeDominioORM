26793566bf75afb23c879065d401b6d455cf8101
westorres9 <wester.torres9@gmail.com>
2022-11-11 11:05:12 -0300

    Entidade Categoria

---
+++ desafioModeloDominioORM/src/main/java/com/desuperior/desafioModeloDominioORM/entities/Categoria.java
@@ -0,0 +1,46 @@
+package com.desuperior.desafioModeloDominioORM.entities;
+
+import java.util.Objects;
+
+public class Categoria {
+
+    private Integer id;
+    private String descricao;
+
+    public Categoria(){
+    }
+
+    public Categoria(Integer id, String descricao) {
+        this.id = id;
+        this.descricao = descricao;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
