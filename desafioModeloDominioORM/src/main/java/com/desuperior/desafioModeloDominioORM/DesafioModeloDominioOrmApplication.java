7f2e3098b9e12b2315a35ae2b70753d59081bece
westorres9 <wester.torres9@gmail.com>
2022-11-11 11:01:40 -0300

    projeto desafio Modelo De Dominio E ORM criado

---
+++ desafioModeloDominioORM/src/main/java/com/desuperior/desafioModeloDominioORM/DesafioModeloDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.desuperior.desafioModeloDominioORM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioModeloDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioModeloDominioOrmApplication.class, args);
+	}
+
+}
---
+++ desafioModeloDominioORM/src/test/java/com/desuperior/desafioModeloDominioORM/DesafioModeloDominioOrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desuperior.desafioModeloDominioORM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioModeloDominioOrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
